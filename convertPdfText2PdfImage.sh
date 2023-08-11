b24a2e58f5cc17926988f79ee4a0797115f53226
Fernando Teubl <16779148+fernandoteubl@users.noreply.github.com>
2023-08-11 15:24:00 -0300

    Update convertPdfText2PdfImage.sh

--- convertPdfText2PdfImage.sh
+++ convertPdfText2PdfImage.sh
@@ -4,6 +4,9 @@ if [ $# -ne 1 ]; then
     echo "Usage:"
     echo "  $(basename "$0") <PDF's file>"
     exit
+elif [[ ! -f "$1" ]]; then
+    echo "The file '$1' didn't exists."
+    exit
 fi
 
 PDF_FILE_INPUT=$1
@@ -15,7 +18,9 @@ PDF_FILE_OUTPUT="$filename"
 mkdir temp
 cp ${PDF_FILE_INPUT} temp
 cd temp
+echo "Step 1/2: generating images of each page of '$PDF_FILE_INPUT'..."
 convert -density 150 ${PDF_FILE_INPUT} -quality 90 output.jpg
+echo "Step 2/2: merging the images into the '$PDF_FILE_OUTPUT'..."
 convert output*.jpg ${PDF_FILE_OUTPUT}
 mv ${PDF_FILE_OUTPUT} ..
 cd ..
